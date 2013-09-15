12caa12d5eb1e4d8e1048c9838b09cc8a0b9cd7c
Josh DeWitt <chessami92@gmail.com>
2013-09-14 22:34:33 -0500

    Added a listener for b.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var a = 1;
 var b = 0;
+var lastDone = function () { encrypt(); };
 
 listener('plaintext', encrypt);
 listener('ciphertext', decrypt);
-listener('a', aListener);
+listener('a', function () { a =smallIntListener('a', true, a); });
+listener('b', function () { b = smallIntListener('b', false, b); });
 
 function flashColor(elementId) {
     var element = document.getElementById(elementId);
@@ -14,32 +16,35 @@ function flashColor(elementId) {
 
 function encrypt() {
     flashColor('ciphertextTd');
+    lastDone = function () { encrypt(); };
 }
 
 function decrypt() {
     flashColor('plaintextTd');
+    lastDone = function () { decrypt(); };
 }
 
-function aListener() {
+function validInt(value) {
     var intRegex = /^\d+$/;
-    var aTd = document.getElementById('aTd');
-    var aValue = document.getElementById('a').value;
-    var aError = document.getElementById('aError');
-    aTd.style.backgroundColor = 'red';
-    if( !intRegex.test(aValue) ) {
-        aError.innerHTML = '\'a\' must be a positive integer.';
-        return;
-    } else if( aValue % 2 == 0 || aValue % 13 == 0 ) {
-        aError.innerHTML = '\'a\' should not have a common multiple with 26.';
-        return;
-    }
-    aTd.style.backgroundColor = 'transparent';
-    aError.innerHTML = '';
-    a = aValue;
+    return intRegex.test(value);
 }
 
-function bListener() {
-    b = document.getElementById('b').value;
+function smallIntListener(prefix, checkLCM, targetValue) {
+    var td = document.getElementById(prefix + 'Td');
+    var value = document.getElementById(prefix).value;
+    var error = document.getElementById(prefix + 'Error');
+    td.style.backgroundColor = 'red';
+    if( !validInt(value) ) {
+        error.innerHTML = '\'' + prefix + '\' must be a positive integer.';
+        return targetValue;
+    } else if( checkLCM == true && ( value % 2 == 0 || value % 13 == 0 ) ) {
+        error.innerHTML = '\'' + prefix + '\' should not have a common multiple with 26.';
+        return targetValue;
+    }
+    td.style.backgroundColor = 'transparent';
+    error.innerHTML = '';
+    lastDone();
+    return value;
 }
 
 function listener(listenTo, handler) {
